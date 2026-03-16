39dc150c298f40e378f1d6ee8028a398a0e1c61b
Zahra Ahmed <zahra.ahmed1017@gmail.com>
2026-03-15 17:58:48 -0700

    adding a bash script for handling the environment issues

---
+++ activate_env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Run: source activate_env.sh
+# Activates the slab-spn poetry venv for this terminal session.
+source /Users/Zahra1/Library/Caches/pypoetry/virtualenvs/slab-spn-qszXI6S5-py3.11/bin/activate
+echo "venv activated: $(which python)"
